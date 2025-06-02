69bc4a83b5d2aa9e553fb8232f0ced1a5e4a9531
davidquintanajob <davidquintanajob@gmail.com>
2025-06-02 04:30:54 -0400

    Cambios para subir la demo

--- services/cerverHost.tsx
+++ services/cerverHost.tsx
@@ -1,5 +1,5 @@
 
-export const cerverHost = "http://localhost:3000";
-export const cerverHostImages = "http://localhost:3000";
+export const cerverHost = "http://31.170.165.44:4000";
+export const cerverHostImages = "http://31.170.165.44:4000";
 // https://my-leniogestionbackend-w5gc.onrender.com
 // http://192.168.1.103 Yunior el del puente yayabo
